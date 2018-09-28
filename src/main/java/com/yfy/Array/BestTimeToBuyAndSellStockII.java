d0cbe843cbcca6296a4f7f25971279ca7012e582
yefangyi <314261471@qq.com>
2018-09-28 09:53:14 +0800

    update the question of 122

---
+++ src/main/java/com/yfy/Array/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,37 @@
+package com.yfy.Array;
+
+public class BestTimeToBuyAndSellStockII {
+
+    public int maxProfit(int[] prices) {
+        int profit = 0, i = 0;
+        while (i < prices.length) {
+            // find next local minimum
+            while (i < prices.length-1 && prices[i+1] <= prices[i]) i++;
+            int min = prices[i++]; // need increment to avoid infinite loop for "[1]"
+            // find next local maximum
+            while (i < prices.length-1 && prices[i+1] >= prices[i]) i++;
+            profit += i < prices.length ? prices[i++] - min : 0;
+        }
+        return profit;
+    }
+
+//    public int maxProfit(int[] prices) {
+//        if(prices.length < 2) {
+//            return 0;
+//        }
+//        int lowest = prices[0], result = 0;
+//        for(int i = 1; i < prices.length - 1; i++) {
+//            if(prices[i] < lowest) {//最低价买入
+//                lowest = prices[i];
+//            }else if(prices[i] > prices[i + 1]) { //卖出
+//                result += prices[i] - lowest;
+//                lowest = prices[i + 1];
+//            }
+//        }
+//        if(prices[prices.length -1] >= prices[prices.length -2]) {
+//            result += prices[prices.length -1] - lowest;
+//        }
+//        return result;
+//    }
+
+}
---
+++ src/test/java/com/yfy/Array/BestTimeToBuyAndSellStockIITest.java
@@ -0,0 +1,29 @@
+package com.yfy.Array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BestTimeToBuyAndSellStockIITest {
+
+    private BestTimeToBuyAndSellStockII bestTimeToBuyAndSellStockII = new BestTimeToBuyAndSellStockII();
+
+    @Test
+    public void case1() {
+        Assert.assertEquals(7, bestTimeToBuyAndSellStockII.maxProfit(new int[]{7,1,5,3,6,4}));
+    }
+
+    @Test
+    public void case2() {
+        Assert.assertEquals(4, bestTimeToBuyAndSellStockII.maxProfit(new int[]{1,2,3,4,5}));
+    }
+
+    @Test
+    public void case3() {
+        Assert.assertEquals(0, bestTimeToBuyAndSellStockII.maxProfit(new int[]{7,6,4,3,1}));
+    }
+
+    @Test
+    public void case4() {
+        Assert.assertEquals(25, bestTimeToBuyAndSellStockII.maxProfit(new int[]{1,9,6,9,1,7,1,1,5,9,9,9}));
+    }
+}
